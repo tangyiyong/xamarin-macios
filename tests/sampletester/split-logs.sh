848eca7c3ae096470e86eb3194d19c1960cba885
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-24 16:59:15 +0200

    [sampletester] Add log splitter.

---
+++ tests/sampletester/split-logs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+#
+# the VSTS log is one giant file
+# this script splits the giant log into one file per test, based on the following line:
+# 2017-08-22T14:42:18.6015150Z => <test name>
+#
+
+if test -z "$1"; then
+	echo "Need to pass the log file to process."
+	exit 1
+fi
+
+LOG=$1
+
+mkdir -p split
+csplit -k -n 4 -f split/log "$LOG" '/ => /' '{9999}'
