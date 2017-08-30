45e70c43df397b11bdb6dd5973418738bee80274
Rolf Bjarne Kvinge <rolf@xamarin.com>
2017-08-30 11:06:41 +0200

    [sampletester] Improve split script.

--- tests/sampletester/split-logs.sh
+++ tests/sampletester/split-logs.sh
@@ -14,4 +14,8 @@ fi
 LOG=$1
 
 mkdir -p split
-csplit -k -n 4 -f split/log "$LOG" '/ => /' '{9999}'
+if [[ "$LOG" == *.zip ]]; then
+	unzip -p "$LOG" 1_Build.txt | csplit -k -n 4 -f split/log - '/ => /' '{9999}'
+else
+	csplit -k -n 4 -f split/log "$LOG" '/ => /' '{9999}'
+fi
